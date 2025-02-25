6b30d4966c250e43604d2654c2d2476d52b31ba7
Yang Hu <yang.h@nyu.edu>
2025-02-25 03:20:34 -0500

    enable customized user table separate sign-up links for different user roles

--- VocationalNYC/users/urls.py
+++ VocationalNYC/users/urls.py
@@ -1,8 +1,21 @@
 from django.urls import path, include
 from django.views.generic import TemplateView
+from allauth.account.views import SignupView
+from .forms import StudentSignupForm, ProviderSignupForm
 from . import views
 
 urlpatterns = [
+    path(
+        "signup/",
+        SignupView.as_view(form_class=StudentSignupForm),
+        name="account_signup",
+    ),
+    # PROVIDER SIGNUP
+    path(
+        "signup/provider/",
+        SignupView.as_view(form_class=ProviderSignupForm),
+        name="account_signup_provider",
+    ),
     path("", include("allauth.urls")),
     path("profile/", TemplateView.as_view(template_name="profile.html"), name="profile"),
     path('provider/<int:pk>/', views.ProviderDetailView.as_view(), name='provider_detail'),
