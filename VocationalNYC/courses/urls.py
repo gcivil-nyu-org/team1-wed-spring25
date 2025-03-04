28801ad86af7a7ef6bd5b17f8462f3e6163b6b64
suwen-susan <sw6359@nyu.edu>
2025-03-04 12:15:57 -0500

    modify database schema and add basic search and filter function

--- VocationalNYC/courses/urls.py
+++ VocationalNYC/courses/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.CourseListView.as_view(), name='course_list'),
     path('<int:pk>/', views.CourseDetailView.as_view(), name='course_detail'),
+    path('search_result/', views.search_result, name='search_result'),
 ]
