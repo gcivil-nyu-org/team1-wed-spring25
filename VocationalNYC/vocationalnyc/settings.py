2eccbbde024f999df015a6b73f75674c05006fd8
vpou18 <victormpou@gmail.com>
2025-04-11 13:19:46 -0400

    access keys via travis

--- VocationalNYC/vocationalnyc/settings.py
+++ VocationalNYC/vocationalnyc/settings.py
@@ -32,7 +32,11 @@ def get_secret(secret_name):
         region_name = "us-east-1"
 
         # Create a Secrets Manager client
-        session = boto3.session.Session()
+        session = boto3.session.Session(
+            aws_access_key_id=env('AWS_ACCESS_KEY_ID'),
+            aws_secret_access_key=env('AWS_SECRET_ACCESS_KEY'),
+            region_name=region_name
+        )
         client = session.client(
             service_name='secretsmanager',
             region_name=region_name
