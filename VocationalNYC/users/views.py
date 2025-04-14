49534a5896c926dc7a3454e59bb87d03f2cb7ced
suwen-susan <sw6359@nyu.edu>
2025-04-14 16:07:56 -0400

    Fix creating default bookmark list bug

--- VocationalNYC/users/views.py
+++ VocationalNYC/users/views.py
@@ -57,7 +57,7 @@ class CustomSignupView(SignupView):
             with transaction.atomic():
                 BookmarkList.objects.create(user=user, name="default")
                 messages.success(
-                    # self.request, "Default bookmark list created successfully."
+                    self.request, "Default bookmark list created successfully."
                 )
         except Exception as e:
             messages.error(
