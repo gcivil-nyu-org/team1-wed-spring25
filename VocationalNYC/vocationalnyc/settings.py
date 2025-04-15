f72b399d8cc3038b6e5d6e42f8c81f9e7d05f1dc
vpou18 <victormpou@gmail.com>
2025-04-15 00:43:15 -0400

    client fix

--- VocationalNYC/vocationalnyc/settings.py
+++ VocationalNYC/vocationalnyc/settings.py
@@ -30,28 +30,28 @@ if env_file.exists():
     environ.Env.read_env(env_file)
 
 def get_secret(secret_name):
-        region_name = "us-east-1"
+    region_name = "us-east-1"
 
-        # Create a Secrets Manager client
-        session = boto3.session.Session()
-        client = session.client(
-            service_name='secretsmanager',
-            region_name=region_name,
-            aws_access_key_id=environ.get('AWS_ACCESS_KEY_ID'),
-            aws_secret_access_key=environ.get('AWS_SECRET_ACCESS_KEY')
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name,
+        aws_access_key_id=env('AWS_ACCESS_KEY_ID'),
+        aws_secret_access_key=env('AWS_SECRET_ACCESS_KEY')
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
         )
+    except ClientError as e:
+        # For a list of exceptions thrown, see
+        # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+        raise e
 
-        try:
-            get_secret_value_response = client.get_secret_value(
-                SecretId=secret_name
-            )
-        except ClientError as e:
-            # For a list of exceptions thrown, see
-            # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
-            raise e
-
-        secret = json.loads(get_secret_value_response['SecretString'])
-        return secret
+    secret = json.loads(get_secret_value_response['SecretString'])
+    return secret
 
 DEBUG = env("DEBUG", default="False")
 
