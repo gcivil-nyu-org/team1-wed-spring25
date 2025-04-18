78ba279899783a33079a2d2a127fc6af5c752cf8
vpou18 <victormpou@gmail.com>
2025-04-18 12:30:18 -0400

    dif approach

--- VocationalNYC/vocationalnyc/settings.py
+++ VocationalNYC/vocationalnyc/settings.py
@@ -37,8 +37,8 @@ def get_secret(secret_name):
     client = session.client(
         service_name='secretsmanager',
         region_name=region_name,
-        aws_access_key_id=env('AWS_ACCESS_KEY_ID'),
-        aws_secret_access_key=env('AWS_SECRET_ACCESS_KEY')
+        aws_access_key_id=environ['AWS_ACCESS_KEY_ID'],
+        aws_secret_access_key=environ['AWS_SECRET_ACCESS_KEY']
     )
 
     try:
