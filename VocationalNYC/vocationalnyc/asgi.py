9d82d62d8e805766ed4bfa3e53240d5421a2bded
Kaifu Xiao <70287441+kxiao02@users.noreply.github.com>
2025-04-27 15:51:54 -0400

    Fix message persistence; streamline ASGI configuration

--- VocationalNYC/vocationalnyc/asgi.py
+++ VocationalNYC/vocationalnyc/asgi.py
@@ -6,21 +6,20 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
-
 import os
+import django
 
-from django.core.asgi import get_asgi_application
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vocationalnyc.settings')
+django.setup()
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
+from django.core.asgi import get_asgi_application
 import message.routing
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vocationalnyc.settings")
-
-application = ProtocolTypeRouter(
-    {
-        "http": get_asgi_application(),
-        "websocket": AuthMiddlewareStack(
-            URLRouter(message.routing.websocket_urlpatterns)
-        ),
-    }
-)
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(message.routing.websocket_urlpatterns)
+    ),
+})
