668763c8518be9f2d442add7d2ca89c9d4539147
Kaifu Xiao <70287441+kxiao02@users.noreply.github.com>
2025-04-30 12:49:50 -0400

    Minor fix

--- VocationalNYC/vocationalnyc/asgi.py
+++ VocationalNYC/vocationalnyc/asgi.py
@@ -15,7 +15,7 @@ django.setup()
 
 from channels.routing import ProtocolTypeRouter, URLRouter  # noqa: E402
 from channels.auth import AuthMiddlewareStack  # noqa: E402
-from channels.security.websocket import AllowedHostsOriginValidator
+from channels.security.websocket import AllowedHostsOriginValidator # noqa: E402
 from django.core.asgi import get_asgi_application  # noqa: E402
 import message.routing  # noqa: E402
 
@@ -23,11 +23,7 @@ application = ProtocolTypeRouter(
     {
         "http": get_asgi_application(),
         "websocket": AllowedHostsOriginValidator(
-            AuthMiddlewareStack(
-                URLRouter(
-                    message.routing.websocket_urlpatterns
-                )
-            )
+            AuthMiddlewareStack(URLRouter(message.routing.websocket_urlpatterns))
         ),
     }
 )
