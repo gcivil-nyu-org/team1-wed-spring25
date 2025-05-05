8ec6d5afba412760e5591c76c196af3c583b5637
Qile (Asher) Zhou <qz2726@nyu.edu>
2025-05-05 14:44:24 -0400

    Update routing.py

--- VocationalNYC/message/routing.py
+++ VocationalNYC/message/routing.py
@@ -3,5 +3,5 @@ from message.consumers import ChatConsumer, ChatListConsumer
 
 websocket_urlpatterns = [
     path("ws/chat/<str:chat_hash>/", ChatConsumer.as_asgi()),
-path("ws/chat_list/", ChatListConsumer.as_asgi()),
+    path("ws/chat_list/", ChatListConsumer.as_asgi()),
 ]
