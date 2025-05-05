a045503dbe69cf4c6002b9364f5206bb75d57a7d
Qile (Asher) Zhou <qz2726@nyu.edu>
2025-05-05 11:57:36 -0400

    Update consumers.py

--- VocationalNYC/message/consumers.py
+++ VocationalNYC/message/consumers.py
@@ -3,6 +3,9 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 from channels.db import database_sync_to_async
 from .models import Chat, Message
 from django.contrib.auth import get_user_model
+from channels.layers import get_channel_layer
+from asgiref.sync import async_to_sync
+from django.utils import timezone
 
 User = get_user_model()
 
@@ -43,6 +46,22 @@ class ChatConsumer(AsyncWebsocketConsumer):
         message = Message.objects.create(
             chat=chat, sender=sender, recipient=recipient, content=message_content
         )
+
+        channel_layer = get_channel_layer()
+        timestamp = timezone.now().isoformat()
+
+        for user in [chat.user1, chat.user2]:
+            async_to_sync(channel_layer.group_send)(
+                f"user_{user.id}_chat_list",
+                {
+                    "type": "chat_list_update",
+                    "data": {
+                        "chat_hash": chat.chat_hash,
+                        "timestamp": timestamp
+                    }
+                }
+            )
+
         return message.pk
 
     async def chat_message(self, event):
@@ -55,3 +74,19 @@ class ChatConsumer(AsyncWebsocketConsumer):
                 }
             )
         )
+
+class ChatListConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        user = self.scope["user"]
+        if user.is_authenticated:
+            self.group_name = f"user_{user.id}_chat_list"
+            await self.channel_layer.group_add(self.group_name, self.channel_name)
+            await self.accept()
+        else:
+            await self.close()
+
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(self.group_name, self.channel_name)
+
+    async def chat_list_update(self, event):
+        await self.send(text_data=json.dumps(event["data"]))
