072319ebf5b3fd8eea2e82788f8c3d14aff3e876
suwen-susan <sw6359@nyu.edu>
2025-05-07 11:36:08 -0400

    Add testcase foe message

--- VocationalNYC/message/consumers.py
+++ VocationalNYC/message/consumers.py
@@ -17,7 +17,6 @@ class ChatConsumer(AsyncWebsocketConsumer):
         await self.accept()
         await self.channel_layer.group_add(self.room_group_name, self.channel_name)
 
-
     async def disconnect(self, close_code):
         await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
 
